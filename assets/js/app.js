4e8a9952df329d5703ef96d266d6c035280e7f85
ryan-barrett <ryan.barrett@spookyhouse.org>
2018-02-07 15:12:24 -0800

    added scrollmagic for setting srolling scenes. Changed to temporary background while resolving seam issues.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,82 @@
-console.log("test");
+$(document).ready(function() {
+  console.log("sanity check");
+
+  var controller = new ScrollMagic.Controller({
+    globalSceneOptions: {
+      duration: $("section").height(),
+      triggerHook: 0.025,
+      reverse: true
+    },
+    vertical: false
+  });
+
+  /*
+  object to hold href values of links inside our nav with
+  the class '.anchor-nav'
+
+  scene_object = {
+    '[scene-name]' : {
+      '[target-scene-id]' : '[anchor-href-value]'
+    }
+  }
+  */
+  var scenes = {
+    intro: {
+      intro: "intro-anchor"
+    },
+    scene2: {
+      "section-1": "anchor1"
+    },
+    scene3: {
+      "section-2": "anchor2"
+    },
+    scene4: {
+      "section-3": "anchor3"
+    }
+  };
+
+  for (var key in scenes) {
+    // skip loop if the property is from prototype
+    if (!scenes.hasOwnProperty(key)) continue;
+
+    var obj = scenes[key];
+
+    for (var prop in obj) {
+      // skip loop if the property is from prototype
+      if (!obj.hasOwnProperty(prop)) continue;
+
+      new ScrollMagic.Scene({ triggerElement: "#" + prop })
+        .setClassToggle("#" + obj[prop], "active")
+        .addTo(controller);
+    }
+  }
+
+  // Change behaviour of controller
+  // to animate scroll instead of jump
+  controller.scrollTo(function(target) {
+    TweenMax.to(window, 0.5, {
+      scrollTo: {
+        x: target,
+        autoKill: true // Allow scroll position to change outside itself
+      },
+      ease: Cubic.easeInOut
+    });
+  });
+
+  //  Bind scroll to anchor links using Vanilla JavaScript
+  var anchor_nav = document.querySelector(".anchor-nav");
+
+  anchor_nav.addEventListener("click", function(e) {
+    var target = e.target,
+      id = target.getAttribute("href");
+
+    if (id !== null && id.length > 0) {
+      e.preventDefault();
+      controller.scrollTo(id);
+
+      if (window.history && window.history.pushState) {
+        history.pushState("", document.title, id);
+      }
+    }
+  });
+});
